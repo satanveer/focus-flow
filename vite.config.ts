c0361f149562b5f0308a34f7cd3e222a96397ad1
Akashdeep Singh <akashdeep.singh49@gmail.com>
2025-09-27 20:04:46 +0530

    Fixes in Vite Config

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      // Enforce single React copy in case parent workspace has another
-      react: '/Users/tanveer/Documents/Projects:Coding/ReactJs/Learn/focusflow/node_modules/react',
-      'react-dom': '/Users/tanveer/Documents/Projects:Coding/ReactJs/Learn/focusflow/node_modules/react-dom'
-    }
-  },
   optimizeDeps: {
     include: ['react', 'react-dom', 'recharts']
   },
